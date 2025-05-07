a73d82260668a59b3ff36b28c4b5a508f13d611b
sabyasachi-padhi <padhisabyasachi354@gmail.com>
2025-05-07 10:48:12 +0530

    Add files via upload

---
+++ day_55/script.js
@@ -0,0 +1,37 @@
+console.log("hey this is tutorial 55")
+
+var a=6;
+var b=6;
+var c="harry"
+
+console.log(a+b)
+// console.log(typeof a,typeof b,typeof c)
+{
+    let a=66;
+    console.log(a)
+}
+
+let x="harry bhai"
+let y=22;
+let z=3.55;
+const p=true;
+let q=undefined;
+let r=null;
+
+ console.log(x,y,z,p,q,r)
+ console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+
+ let o={
+    "name":"harry",
+    "job role":5600
+
+    
+ }
+ console.log(o);
+
+ o.salary="100crores";
+ console.log(o);
+ o.salary="500crores";
+ console.log(o);
+
+ 
