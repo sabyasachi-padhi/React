d5ef35a6b7c2757505fe53a5c9d0582acd4c6490
sabyasachi-padhi <padhisabyasachi354@gmail.com>
2025-05-07 10:39:05 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Chai aur react | i have learnt react basic to advance form hitesh sir's chai aur code channel hitesh sir was very good teacher.he studied in depth and in details 
+        in react .we learn that how react work in browser.using html and js because browser
+        can not understand react.print our first word.I hope that the playlist was very 
+        helpful to us.
+      </h1>
+    </>
+  )
+}
+
+export default App
